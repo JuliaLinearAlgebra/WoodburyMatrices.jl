c9fa159797c9cf806c2730661d9bb94a0dfb0632
Tim Holy <tim.holy@gmail.com>
2015-02-02 13:23:50 -0600

    First real commit
    This is code just copied from base, in preparation for #10024.
    Originally written by Tim Holy but maintained by Andreas Noack Jensen,
    rickhg12hs, and others.

--- README.md
+++ README.md
@@ -1,3 +1,18 @@
 # WoodburyMatrices
 
 [![Build Status](https://travis-ci.org/timholy/WoodburyMatrices.jl.svg?branch=master)](https://travis-ci.org/timholy/WoodburyMatrices.jl)
+
+This package provides support for the [Woodbury matrix identity](http://en.wikipedia.org/wiki/Woodbury_matrix_identity) for the Julia programming language.  This is a generalization of the [Sherman-Morrison formula](http://en.wikipedia.org/wiki/Sherman%E2%80%93Morrison_formula).
+
+## Usage
+
+```julia
+using WoodburyMatrices
+W = Woodbury(A, U, C, V)
+```
+creates a `Woodbury` matrix from the `A`, `U`, `C`, and `V` matrices.
+
+There are only a few things you can do with a Woodbury matrix:
+- `full(W)` converts to its dense representation
+- `W\b` solves the equation `W*x = b` for `x`. Note that the Woodbury matrix identity is notorious for floating-point roundoff errors, so be prepared for a certain amount of inaccuracy in the result.
+- `det(W)` computes the determinant of `W`.
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,57 @@
 using WoodburyMatrices
 using Base.Test
 
-# write your own tests here
-@test 1 == 1
+srand(123)
+n = 5
+
+d = 1 .+ rand(n)
+dl = -rand(n-1)
+du = -rand(n-1)
+v = randn(n)
+B = randn(n,2)
+
+U = randn(n,2)
+V = randn(2,n)
+C = randn(2,2)
+
+for elty in (Float32, Float64, Complex64, Complex128, Int)
+    if elty == Int
+        srand(61516384)
+        d = rand(1:100, n)
+        dl = -rand(0:10, n-1)
+        du = -rand(0:10, n-1)
+        v = rand(1:100, n)
+        B = rand(1:100, n, 2)
+
+        # Woodbury
+        U = rand(1:100, n, 2)
+        V = rand(1:100, 2, n)
+        C = rand(1:100, 2, 2)
+    else
+        d = convert(Vector{elty}, d)
+        dl = convert(Vector{elty}, dl)
+        du = convert(Vector{elty}, du)
+        v = convert(Vector{elty}, v)
+        B = convert(Matrix{elty}, B)
+        U = convert(Matrix{elty}, U)
+        V = convert(Matrix{elty}, V)
+        C = convert(Matrix{elty}, C)
+    end
+    ε = eps(abs2(float(one(elty))))
+    T = Tridiagonal(dl, d, du)
+    @test size(T, 1) == n
+    @test size(T) == (n, n)
+
+    # Woodbury
+    W = Woodbury(T, U, C, V)
+    F = full(W)
+    @test_approx_eq W*v F*v
+    iFv = F\v
+    @test norm(W\v - iFv)/norm(iFv) <= n*cond(F)*ε # Revisit. Condition number is wrong
+    @test abs((det(W) - det(F))/det(F)) <= n*cond(F)*ε # Revisit. Condition number is wrong
+    iWv = similar(iFv)
+    if elty != Int
+        iWv = A_ldiv_B!(W, copy(v))
+        @test_approx_eq iWv iFv
+    end
+end
